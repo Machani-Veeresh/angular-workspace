99fa7a5f68c033a6a01f748230a8bcd9549d461a
Machani-Veeresh <veeresh@sixberries.com>
2022-11-05 14:15:12 +0530

    Expression & Interpolations

--- src/app/hello-world.component.ts
+++ src/app/hello-world.component.ts
@@ -2,7 +2,12 @@ import { Component,OnInit,OnDestroy } from '@angular/core';
 
 @Component({
     selector: 'app-hello-world',
-    template: '<h1>{{title}}</h1>',
+    template: `
+    // Text Interpolation Example 
+    <h1> 1.  {{title}}</h1>
+    // Expressions Example
+    <div>2. {{number + number }}</div>
+    <div>{{getMaximum(2,4)}}</div>`,
     styles: [`
    h1 {
     color: red;
@@ -11,15 +16,24 @@ import { Component,OnInit,OnDestroy } from '@angular/core';
 })
 export class HelloWorldComponent implements OnInit, OnDestroy{
     interValSlab: string | number | NodeJS.Timer | undefined;
+    title = 'Hello World';
+    number = 1;
     ngOnInit(): void {
-        this.interValSlab = setInterval(()=>{
+        console.log('onInit');
+       /*  this.interValSlab = setInterval(()=>{
             console.log('Hello from NgOnit')
-        },1000)
+        },1000) */
+    }
+    getMaximum(a: number,b: number) {
+        if(a>b){
+            return a;
+        }else {
+            return b;
+        }
     }
     ngOnDestroy(): void {
         if(this.interValSlab){
             clearInterval(this.interValSlab);
         }
     }
-    title = 'Hello World'
 }
